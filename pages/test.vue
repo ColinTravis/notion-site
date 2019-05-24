8a7cfbf3c57e5ea9a9081106688f6afdfcc82bdf
Colin Travis <colin.travis@collemcvoy.com>
2019-05-24 14:37:48 -0500

    Changed page

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <div class="container">
+            <h1 class="text-bold"> Hello </h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+head: {
+    title: "Now Test!"
+}
+}
+</script>
+
+<style>
+
+</style>
