313380d62fd03444e0ff81e764094bfb7b7140c6
Colin Travis <colin.travis@collemcvoy.com>
2019-05-29 09:43:49 -0500

    New page ID

--- plugins/notion.js
+++ plugins/notion.js
@@ -1,67 +1,67 @@
 // import fetch from "isomorphic-unfetch";
 
 // const PAGE_ID = "1a86e7f6-d6a5-4537-a2e5-15650c1888b8";
-const PAGE_ID = "d1c555e0-3886-4c56-b09a-4a4bb879b841";
+const PAGE_ID = "654c0296-7d0b-4587-a64d-91b57d870430";
 
 export default async function getNotionData() {
-  const data = await loadPageChunk({ pageId: PAGE_ID });
-  const blocks = values(data.recordMap.block);
+  const data = await loadPageChunk({ pageId: PAGE_ID })
+  const blocks = values(data.recordMap.block)
 
-  const sections = [];
-  let meta = {};
+  const sections = []
+  let meta = {}
 
-  let currentSection = null;
+  let currentSection = null
 
   for (const block of blocks) {
-    const value = block.value;
+    const value = block.value
 
     if (
-      value.type === "page" ||
-      value.type === "header" ||
-      value.type === "sub_header"
+      value.type === 'page' ||
+      value.type === 'header' ||
+      value.type === 'sub_header'
     ) {
-      sections.push({ title: value.properties.title, children: [] });
-      continue;
+      sections.push({ title: value.properties.title, children: [] })
+      continue
     }
 
-    const section = sections[sections.length - 1];
-    let list = null;
+    const section = sections[sections.length - 1]
+    let list = null
 
-    if (value.type === "image") {
-      list = null;
+    if (value.type === 'image') {
+      list = null
       const child = {
-        type: "image",
+        type: 'image',
         src: `/image.js?url=${encodeURIComponent(value.format.display_source)}`
-      };
-      section.children.push(child);
-    } else if (value.type === "text") {
-      list = null;
+      }
+      section.children.push(child)
+    } else if (value.type === 'text') {
+      list = null
       if (value.properties) {
         section.children.push({
-          type: "text",
+          type: 'text',
           value: value.properties.title
-        });
+        })
       }
-    } else if (value.type === "bulleted_list") {
+    } else if (value.type === 'bulleted_list') {
       if (list == null) {
         list = {
-          type: "list",
+          type: 'list',
           children: []
-        };
-        section.children.push(list);
+        }
+        section.children.push(list)
       }
-      list.children.push(value.properties.title);
-    } else if (value.type === "collection_view") {
+      list.children.push(value.properties.title)
+    } else if (value.type === 'collection_view') {
       const col = await queryCollection({
         collectionId: value.collection_id,
         collectionViewId: value.view_ids[0]
-      });
-      const table = {};
+      })
+      const table = {}
       const entries = values(col.recordMap.block).filter(
         block => block.value && block.value.parent_id === value.collection_id
-      );
+      )
       for (const entry of entries) {
-        const props = entry.value.properties;
+        const props = entry.value.properties
 
         // I wonder what `Agd&` is? it seems to be a fixed property
         // name that refers to the value
@@ -69,25 +69,25 @@ export default async function getNotionData() {
           props.title[0][0]
             .toLowerCase()
             .trim()
-            .replace(/[ -_]+/, "_")
-        ] = props["Agd&"];
+            .replace(/[ -_]+/, '_')
+        ] = props['Agd&']
       }
 
       if (sections.length === 1) {
-        meta = table;
+        meta = table
       } else {
         section.children.push({
-          type: "table",
+          type: 'table',
           value: table
-        });
+        })
       }
     } else {
-      list = null;
-      console.log("UNHANDLED", value);
+      list = null
+      console.log('UNHANDLED', value)
     }
   }
 
-  return { sections, meta };
+  return { sections, meta }
 }
 
 async function rpc(fnName, body = {}) {
